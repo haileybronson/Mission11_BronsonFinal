8ff01bc0c497912259fc670b6296fb75258ab32b
Hailey Bronson <haileyp7@byu.edu>
2024-03-20 10:33:52 -0600

    pagination process continued

--- Mission11_Bronson/Controllers/HomeController.cs
+++ Mission11_Bronson/Controllers/HomeController.cs
@@ -14,13 +14,24 @@ public class HomeController : Controller
 
     public IActionResult Index(int pageNum)
     {
-        int pageSize = 10; 
+        int pageSize = 10;
 
-        var bookstoreData = _repo.Books
+        var blah = new BooksListViewModel
+        {
+            Books = _repo.Books
             .OrderBy(x=> x.Title)
             .Skip((pageNum - 1) * pageSize)
-            .Take(pageSize);
-        return View(bookstoreData);
+            .Take(pageSize),
+
+            PaginationInfo = new PaginationInfo
+            {
+                CurrentPage = pageNum, 
+                ItemsPerPage = pageSize, 
+                TotalItems = _repo.Books.Count()
+            }
+        };
+            
+        return View(blah);
     }
 
 }
