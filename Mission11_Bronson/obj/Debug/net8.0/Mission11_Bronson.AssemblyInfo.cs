a194b738aca0506cf0b8fcfa2ba51f6dc2a8aaf8
haileybronson <haileybronsonphotos@gmail.com>
2024-04-05 18:00:49 -0600

    finishing touches, will now move and continue to intex project

--- Mission11_Bronson/obj/Debug/net8.0/Mission11_Bronson.AssemblyInfo.cs
+++ Mission11_Bronson/obj/Debug/net8.0/Mission11_Bronson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Bronson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff84a782a07d52171f551f0d50a19acc83ad570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e616c83e2a5c13909b2f7311e4b5ac60a83a14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Bronson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Bronson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
